d1b9543acca3e37ceceece134fc2848080e3c327
dlatldhs <sionzz1713@gmail.com>
2024-04-18 09:01:06 +0900

    fix :: kakao async

--- src/components/Kakao.js
+++ src/components/Kakao.js
@@ -6,14 +6,19 @@ const {kakao} = window;
 const Kakao = () => {
     
     useEffect(() => {
-        const container = document.getElementById('map');
-        const options = {
-            center: new kakao.maps.LatLng(35.1605, 128.8641),
-            level: 3
+        const initializeMap = async () => {
+            const container = document.getElementById('map');
+            const options = {
+                center: new kakao.maps.LatLng(35.1608594, 128.8675003),
+                level: 3
+            };
+            
+            const map = new kakao.maps.Map(container, options);
         };
-        
-        const map = new kakao.maps.Map(container, options);
-    }, [])
+
+        initializeMap();
+    }, []);
+
     // Lat: 35.1605 Lon: 128.8641
     return (
         <div id="map" style={{
@@ -24,4 +29,32 @@ const Kakao = () => {
     )
 };
 
-export default Kakao; 
+export default Kakao;
+
+// import React, { useEffect } from 'react';
+// import styled from 'styled-components';
+
+// const {kakao} = window;
+
+// const Kakao = () => {
+    
+//     useEffect(() => {
+//         const container = document.getElementById('map');
+//         const options = {
+//             center: new kakao.maps.LatLng(35.1608594, 128.8675003),
+//             level: 3
+//         };
+        
+//         const map = new kakao.maps.Map(container, options);
+//     }, [])
+//     // Lat: 35.1605 Lon: 128.8641
+//     return (
+//         <div id="map" style={{
+//             width: '600px',
+//             height:'400px',
+//             marginRight: '150px'
+//         }}></div>
+//     )
+// };
+
+// export default Kakao; 
