7add625d1df5044846fafe0e26ff46c56f4af8f7
dlatldhs <sionzz1713@gmail.com>
2024-04-18 09:16:44 +0900

    fix kakao api error

--- src/components/Kakao.js
+++ src/components/Kakao.js
@@ -1,60 +1,30 @@
+/* global kakao */
 import React, { useEffect } from 'react';
-import styled from 'styled-components';
 
-const {kakao} = window;
-
-const Kakao = () => {
-    
+const KakaoMap = () => {
     useEffect(() => {
-        const initializeMap = async () => {
-            const container = document.getElementById('map');
-            const options = {
-                center: new kakao.maps.LatLng(35.1608594, 128.8675003),
-                level: 3
+        const loadKakaoMap = () => {
+            const script = document.createElement('script');
+            script.async = true;
+            script.src = "https://dapi.kakao.com/v2/maps/sdk.js?appkey=e8f1677269dd57608c41205f9e55842f&autoload=false";
+            document.head.appendChild(script);
+
+            script.onload = () => {
+                kakao.maps.load(() => {
+                    const container = document.getElementById('map');
+                    const options = {
+                        center: new kakao.maps.LatLng(35.1608594, 128.8675003),
+                        level: 3,
+                    };
+                    const map = new kakao.maps.Map(container, options);
+                });
             };
-            
-            const map = new kakao.maps.Map(container, options);
         };
 
-        initializeMap();
+        loadKakaoMap();
     }, []);
 
-    // Lat: 35.1605 Lon: 128.8641
-    return (
-        <div id="map" style={{
-            width: '600px',
-            height:'400px',
-            marginRight: '150px'
-        }}></div>
-    )
+    return <div id="map" style={{ width: '600px', height: '400px', marginRight: '150px' }}></div>;
 };
 
-export default Kakao;
-
-// import React, { useEffect } from 'react';
-// import styled from 'styled-components';
-
-// const {kakao} = window;
-
-// const Kakao = () => {
-    
-//     useEffect(() => {
-//         const container = document.getElementById('map');
-//         const options = {
-//             center: new kakao.maps.LatLng(35.1608594, 128.8675003),
-//             level: 3
-//         };
-        
-//         const map = new kakao.maps.Map(container, options);
-//     }, [])
-//     // Lat: 35.1605 Lon: 128.8641
-//     return (
-//         <div id="map" style={{
-//             width: '600px',
-//             height:'400px',
-//             marginRight: '150px'
-//         }}></div>
-//     )
-// };
-
-// export default Kakao; 
+export default KakaoMap;
