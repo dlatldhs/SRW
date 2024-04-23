9b851d0682a59b32a6604e9086ec98402d7a4748
dlatldhs <sionzz1713@gmail.com>
2024-04-23 09:42:42 +0900

    last test

--- web.js
+++ web.js
@@ -2,7 +2,7 @@
 let express = require("express");
 const path = require("path")
 const app = express();
-const port = 3000
+const port = 8001
 
 app.use(express.static(path.join(__dirname, '/build'))); // react를 빌드한 결과물이 담긴 디렉토리 /build 에 접근하여 미들웨어를 생성해준다.
 
@@ -26,6 +26,6 @@ app.get("/contacts", (req, res) => {
     res.sendFile(path.join(__dirname, "/build/index.html"));
 })
 
-// app.listen(port,()=>{
-//     console.log(`example test port ${port}`);
-// })
+app.listen(port,()=>{
+    console.log(`example test port ${port}`);
+})
