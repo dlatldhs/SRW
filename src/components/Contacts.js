29f73ffbfe45518c283ffd76dcb2a26edbc9bdc0
dlatldhs <sionzz1713@gmail.com>
2024-05-10 00:42:04 +0900

    FIX :: Design

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,173 +1,198 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import Header from './Header';
 import Footer from './Footer';
 import Kakao from './Kakao';
-import FormfacadeEmbed from "@formfacade/embed-react";
+
+// 반응형 스타일을 위한 테마 설정
+const theme = {
+  sizes: {
+    mobile: 480,
+    tablet: 768,
+    desktop: 1024,
+  },
+  media: {
+    mobile: (...args) => `@media (max-width: ${theme.sizes.mobile}px) {
+      ${args}
+    }`,
+    tablet: (...args) => `@media (min-width: ${theme.sizes.mobile + 1}px) and (max-width: ${theme.sizes.tablet}px) {
+      ${args}
+    }`,
+    desktop: (...args) => `@media (min-width: ${theme.sizes.tablet + 1}px) {
+      ${args}
+    }`,
+  },
+};
 
 const ContactsBackground = styled.div`
-    background-color: white;
-    height: 180vh;
-    max-height: 270vh;
-    width: 100%;
-    max-width: 100vw;
-    // background-image: url('/img/background_1.jpg');
-    // background-repeat: repeat;
+  background-color: white;
+  height: 100vh;
+  max-height: 100vh;
+  width: 100%;
+  max-width: 100vw;
 `;
 
 const ContactsPage = styled.div`
-    // display: flex;
-    // justify-content: center;
-    // align-items: center;
-    // width: 100%;
-    // height: 60vh;
-    // max-width: 100vw;
-    // max-height:100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 90vh;
+  max-width: 100vw;
+  max-height: 90vh;
 
-    // display: flex;
-    // flex-direction: column;
-    // justify-content: center;
-    // align-items: center;
-    // width: 100%;
-    // height: 70vh;
-    // max-width: 100vw;
-    // max-height:100vh;
+  ${theme.media.mobile`
+    height: auto;
+    max-height: none;
+  `}
+`;
 
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100vh;
-    max-width: 100vw;
-    max-height:100vh;
+const MapInfoTitle = styled.h2`
+  margin: 0;
+  text-align: center;
+  width: 100%;
+  background-color: #c0c0c0;
+  color: white; // 글자 색을 흰색으로 변경
+  padding: 10px 0;
+  font-size: 1.5rem;
 
+  ${theme.media.mobile`
+    font-size: 1.2rem;
+  `}
 `;
 
 const CustomerConnect = styled.div`
-    // display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    max-width: 100vw;
-    max-height:200vh;
-    position: relative; /* 여기에 추가 */
-    
-    overflow-y: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 90vh;
+  max-width: 100vw;
+  max-height: 90vh;
+  position: relative;
+  overflow-y: hidden;
+
+  ${theme.media.mobile`
+    height: auto;
+    max-height: none;
+  `}
 `;
 
 const Title = styled.div`
-    margin: 0;
-    text-align: center;
-    // position: absolute;
-    top: 0;
-    width: 100%;
-    height: 44px;
-    line-height: 44px;
-    background-color: #c0c0c0;
-    margin-bottom: 20px;
+  margin: 0;
+  text-align: center;
+  top: 0;
+  width: 100%;
+  height: 44px;
+  line-height: 44px;
+  background-color: #c0c0c0;
+  margin-bottom: 20px;
 `;
 
-const FormWrapper = styled.div`
-    width: calc(100vw - 30px); // 전체 뷰포트 너비에서 원하는 만큼의 padding 값을 빼줍니다.
-    max-width: 100vw; // 최대 너비 설정
-    padding: 0 15px; // 양쪽으로 padding을 줍니다.
+const MapInfoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  height: 80%;
+  margin: 10% 10%;
 
-    // 미디어 쿼리를 사용하여 화면 크기에 따른 스타일 조정
-    @media (max-width: 768px) {
-        width: calc(100vw - 20px); // 모바일 화면에서는 더 적은 padding을 적용할 수 있습니다.
-        padding: 0 10px;
-    }
-    @media (max-width: 480px) {
-        width: 100vw; // 가장 작은 화면에서는 padding 없이 화면 전체를 사용합니다.
-        padding: 0;
-    }
+  ${theme.media.mobile`
+    flex-direction: column;
+    height: auto;
+    margin: 0;
+  `}
 `;
 
 const MapInfo = styled.div`
-    // max-height: 400px;
-    // max-width: 600px;
-    // height: 100%;
-    // width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 30%;
 
-    // flex: 0.2; // MapInfo가 20% 차지
-    // height: 100%;
-    // display: flex;
-    // flex-direction: column;
-    // justify-content: center;
-    // align-items: center;
-
-    flex: 0.2; // MapInfo가 20% 차지
-    height: 100%;
-    // display: flex;
-    // flex-direction: column;
-    // justify-content: center;
-    align-items: center;
+  ${theme.media.mobile`
+    width: 100%;
+    height: auto;
+  `}
 `;
 
-const Info = styled.div`
-    // & > *:not(:last-child) { /* Info 컴포넌트의 모든 직접적인 자식에 대해, 마지막 자식을 제외하고 적용 */
-    //     margin-bottom: 10px; /* 또는 원하는 간격에 맞춰 조절 */
-    // }
-    text-align: center;
-    margin-bottom: 20px;
+const KakaoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 70%;
+
+  ${theme.media.mobile`
+    width: 100%;
+    height: auto;
+  `}
 `;
 
 const PictureInfo = styled.p`
-    // max-height: 331px;
-    // max-width: 600px;
-    // height: 100%;
-    // width: 100%;
-    // background-color: pink;
+  width: 100%;
+  height: 100%;
+  background-color: #f0f0f0;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.2rem;
+  padding: 20px;
 
-    width: 100vw;
-    height: 100%;
-    background-color: #f0f0f0;
-    text-align: center;
-    display: table-cell;
-    vertical-align: middle;
+  ${theme.media.mobile`
+    font-size: 1rem;
+  `}
 `;
-
-const InfoTitle = styled.h2`
-    margin: 0 0 0 0;
-
-`;
-
-
 
 const Contacts = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <ContactsBackground>
+        <Header />
 
-    // const url="https://formfacade.com/include/116703339410389346194/form/1FAIpQLSfaaocgdeO4E_ffGB5l6VOqzfiUglHXeCxaRCAjZETqSS92rw/classic.js/?div=ff-compose";
-    // const url="https://formfacade.com/include/106597252125648279836/form/1FAIpQLSeWHmxuJOaVLxTrZEv5kIWsLLCdkc-L00XHHqqDs8MWYYc0UA/classic.js/?div=ff-compose"
-    const url="https://formfacade.com/include/105207456793860182179/form/1FAIpQLSfe6zQIt4Io-EoKjeco-bKNMAcLGOzLtIgKtglwkvfM_RoY_Q/classic.js/?div=ff-compose"
-    // const url="https://formfacade.com/include/116703339410389346194/form/1FAIpQLSfMgp0--jltWUwshAiPrkVMFDGSxG6Th-FlcP0qs9hIXqd4_w/classic.js/?div=ff-compose"
-    return (
-        <ContactsBackground>
-            <Header />
-            
-            <CustomerConnect>
-                <Title>고객 문의</Title>
-                {/* <FormfacadeEmbed
-                    formFacadeURL={url}
-                    onSubmitForm={() => console.log('Form submitted')}
-                /> */}
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfO_K3RS0UlUuZo_kJEHB1mP1YAC-GhhqPZAoQ5G_ggHjebyA/viewform?embedded=true" width="100%" height="2787" frameborder="0" marginheight="0" marginwidth="0">로드 중…</iframe>    
-            </CustomerConnect>      
+        <CustomerConnect>
+          <Title>고객 문의</Title>
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSfO_K3RS0UlUuZo_kJEHB1mP1YAC-GhhqPZAoQ5G_ggHjebyA/viewform?embedded=true"
+            width="100%"
+            height="100%"
+            frameBorder="0"
+            marginHeight="0"
+            marginWidth="0"
+          >
+            로드 중…
+          </iframe>
+        </CustomerConnect>
 
-            <ContactsPage>
-                <Kakao />
-                <MapInfo>
-                    <Info>
-                        <InfoTitle>Contacts</InfoTitle> 
-                    </Info>
-                    <PictureInfo>부산사무실 : 부산광역시 강서구 미음산단5로41번길 77<br></br>부산 공장 : 부산광역시 강서구 미음산단4로 32<br></br>전화번호 055) 362-9992<br></br>이메일 starobt@naver.com</PictureInfo>
-                </MapInfo>
-            </ContactsPage>
+        <ContactsPage>
+          <MapInfoTitle>찾아오시는 길</MapInfoTitle>
+          <MapInfoContainer>
+            <MapInfo>
+              <PictureInfo>
+                부산사무실 : 부산광역시 강서구 미음산단5로41번길 77
+                <br />
+                부산 공장 : 부산광역시 강서구 미음산단4로 32
+                <br />
+                전화번호 055) 362-9992
+                <br />
+                이메일 starobt@naver.com
+              </PictureInfo>
+            </MapInfo>
+            <KakaoContainer>
+              <Kakao />
+            </KakaoContainer>
+          </MapInfoContainer>
+        </ContactsPage>
 
-            <Footer />
-        </ContactsBackground>
-    );
+        <Footer />
+      </ContactsBackground>
+    </ThemeProvider>
+  );
 };
 
 export default Contacts;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,110 @@
+// import React from 'react';
+// import styled from 'styled-components';
+// import { useNavigate } from 'react-router-dom';
+
+// // 스타일이 적용된 컴포넌트들
+// const StyledHeader = styled.header`
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   height: 10vh;
+//   position: relative;
+//   background-color: #ffffff;
+// `;
+
+// const Logo = styled.img`
+//   max-width: 230px;
+//   width: 100%;
+//   max-height: 64px;
+//   height: 100%;
+//   // background-color: red;
+//   left: 120px;
+//   position: absolute;
+//   cursor: pointer;
+// `;
+
+// const Navigation = styled.nav`
+//   position: absolute;
+//   right: 50px;
+//   max-width: 600px;
+//   max-height: 50px;
+
+//   width: 100%;
+//   height: 100%;
+
+//   display: flex;
+//   justify-content: space-around;
+  
+//   a {
+//     text-decoration: none;
+//     color: black;
+//     outline: none;
+
+//     width: 100%;
+//     height: 100%;
+
+//     max-width: 150px;
+//     max-height: 50px;
+    
+//     display: block;
+
+//     text-align: center;
+//     align-content: center;
+
+//     font-size: 17px;
+
+//     &:hover {
+//       background-color: gray; opacity : 0.5;
+//       color: white;
+//     }
+//   }
+// `;
+
+// const Header = () => {
+
+//   const navigate = useNavigate();
+
+//   const handleClickLogo = () => {
+//     navigate('/')
+//   }
+
+//   return (
+//     <StyledHeader>
+//       <Logo onClick={handleClickLogo} src="/img/image_6.png" alt="LOGO"></Logo>
+//       <Navigation>
+//         <a href="company">회사소개</a>
+//         <a href="business">사업소개</a>
+//         {/* <a href="skills">서비스</a> */}
+//         <a href="contacts">Contacts</a>
+//       </Navigation>
+//     </StyledHeader>
+//   );
+// }
+
+// export default Header;
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { useNavigate } from 'react-router-dom';
+
+// 반응형 스타일링을 위한 미디어 쿼리 설정
+const theme = {
+  sizes: {
+    mobile: 480,
+    tablet: 768,
+    desktop: 1024,
+  },
+  media: {
+    mobile: (...args) => `@media (max-width: ${theme.sizes.mobile}px) {
+      ${args}
+    }`,
+    tablet: (...args) => `@media (min-width: ${theme.sizes.mobile + 1}px) and (max-width: ${theme.sizes.tablet}px) {
+      ${args}
+    }`,
+    desktop: (...args) => `@media (min-width: ${theme.sizes.tablet + 1}px) {
+      ${args}
+    }`,
+  },
+};
 
 // 스타일이 적용된 컴포넌트들
 const StyledHeader = styled.header`
@@ -17,68 +121,122 @@ const Logo = styled.img`
   width: 100%;
   max-height: 64px;
   height: 100%;
-  // background-color: red;
   left: 120px;
   position: absolute;
   cursor: pointer;
+
+  ${theme.media.mobile`
+    max-width: 150px;
+    left: 20px;
+  `}
+
+  ${theme.media.tablet`
+    max-width: 180px;
+    left: 50px;
+  `}
 `;
+
+// const Navigation = styled.nav`
+//   position: absolute;
+//   right: 50px;
+//   max-width: 600px;
+//   max-height: 50px;
+//   width: 100%;
+//   height: 100%;
+//   display: flex;
+//   justify-content: space-around;
+
+//   a {
+//     text-decoration: none;
+//     color: black;
+//     outline: none;
+//     width: 100%;
+//     height: 100%;
+//     max-width: 100px;
+//     max-height: 50px;
+//     display: block;
+//     text-align: center;
+//     align-content: center;
+//     font-size: 17px;
+
+//     &:hover {
+//       background-color: gray;
+//       opacity: 0.5;
+//       color: white;
+//     }
+
+//     ${theme.media.mobile`
+//       font-size: 14px;
+//       max-width: 100px;
+//     `}
+
+//     ${theme.media.tablet`
+//       font-size: 16px;
+//       max-width: 120px;
+//     `}
+//   }
+// `;
 
 const Navigation = styled.nav`
   position: absolute;
   right: 50px;
-  max-width: 600px;
+  max-width: 400px;
   max-height: 50px;
-
   width: 100%;
   height: 100%;
-
   display: flex;
-  justify-content: space-around;
-  
+  justify-content: space-between;
+  gap: 20px; // 모바일/태블릿 화면에서 a 태그 사이의 간격 조정
+
   a {
     text-decoration: none;
     color: black;
     outline: none;
-
     width: 100%;
     height: 100%;
-
-    max-width: 150px;
+    max-width: 150px; // 모바일/태블릿 화면에서 a 태그 최대 너비 조정
     max-height: 50px;
-    
     display: block;
-
     text-align: center;
     align-content: center;
-
     font-size: 17px;
 
     &:hover {
-      background-color: gray; opacity : 0.5;
+      background-color: gray;
+      opacity: 0.5;
       color: white;
     }
+
+    ${theme.media.mobile`
+      font-size: 14px;
+      max-width: 100px;
+    `}
+
+    ${theme.media.tablet`
+      font-size: 16px;
+      max-width: 120px;
+    `}
   }
 `;
 
-const Header = () => {
 
+const Header = () => {
   const navigate = useNavigate();
 
   const handleClickLogo = () => {
-    navigate('/')
-  }
+    navigate('/');
+  };
 
   return (
     <StyledHeader>
-      <Logo onClick={handleClickLogo} src="/img/image_6.png" alt="LOGO"></Logo>
+      <Logo onClick={handleClickLogo} src="/img/image_6.png" alt="LOGO" />
       <Navigation>
         <a href="company">회사소개</a>
         <a href="business">사업소개</a>
-        {/* <a href="skills">서비스</a> */}
         <a href="contacts">Contacts</a>
       </Navigation>
     </StyledHeader>
   );
-}
+};
 
 export default Header;
